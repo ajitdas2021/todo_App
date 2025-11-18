786203dd836d333c8df690c2b83fd2f98df30cb3
ajitdas2021 <ajitdasku2021@gmail.com>
2025-11-18 16:05:14 +0545

    ..

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -68,6 +68,8 @@
 //     );
 //   }
 // }
+// this is main.dart
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
